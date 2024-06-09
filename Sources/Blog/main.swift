315bc2a9614a94a8a9018df2e61f0bbe2910fac1
Anton Priakhin <apriakhin@aol.com>
2024-06-09 23:27:46 +0300

    first commit

---
+++ Sources/Blog/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Publish
+import Plot
+
+struct Blog: Website {
+  enum SectionID: String, WebsiteSectionID {
+    case posts
+  }
+
+  struct ItemMetadata: WebsiteItemMetadata {}
+
+  var url = URL(string: "https://blog.apriakhin.ru")!
+  var name = "Антон Пряхин"
+  var description = "Блог о iOS разработке"
+  var language: Language { .russian }
+  var imagePath: Path? { Path("/images/avatar.jpg") }
+}
+
+try Blog().publish(
+  withTheme: .foundation,
+  deployedUsing: .gitHub(
+    "",
+    branch: "gh-pages"
+  )
+)
